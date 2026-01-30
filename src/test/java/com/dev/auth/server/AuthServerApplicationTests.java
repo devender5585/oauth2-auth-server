4faccd7f13603c9385c24837ad49eac0cbab26b4
Your Name <todevenderkapil@gmail.com>
2026-01-30 21:39:54 +0530

    Initialize OAuth2 Authorization Server project with Spring Boot

---
+++ src/test/java/com/dev/auth/server/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
